69c65cc8a5dfe5d0369fddc60b893b64e34b7bf9
Manuel Hohenadl <hm@campudus.com>
2023-07-03 10:31:33 +0200

    clean up & add meta for responsive

--- src/models/dbModels.tsx
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const Schema = new mongoose.Schema({
-  hours: {
-    type: Number
-  }
-});
-
-export default mongoose.models.dbModels || mongoose.model("dbModels", Schema);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import "../styles/index.scss";
 import type { AppProps } from "next/app";
-import { SWRConfig } from "swr";
-
 import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
